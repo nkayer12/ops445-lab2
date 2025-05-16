89067008ed70ec502878159b458568e4430567ae
Narendra Ayer <nkayer@myseneca.ca>
2025-05-16 13:55:02 -0400

    Added lab2f

---
+++ lab2f.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Author: Narendra Ayer
+# Author ID: nkayer
+# Date Created: 2025/05/16
+
+import sys
+
+timer = int(sys.argv[1])
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
+
