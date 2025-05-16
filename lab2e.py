8b2aeeb42421692879a667b392db8f8209d0771c
Narendra Ayer <nkayer@myseneca.ca>
2025-05-16 13:50:46 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Narendra Ayer
+# Author ID: nkayer
+# Date Created: 2025/05/16
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
